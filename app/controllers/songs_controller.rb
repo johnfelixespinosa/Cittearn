ebd10925da477fced91b66ad9894d55b49905dfd
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-10-06 14:08:00 -0400

    Can create songs, new and show paths

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,31 @@
 class SongsController < ApplicationController
+
+  def new 
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      # session[:user_id] = @user.id
+      redirect_to song_path(@song)
+    else
+      redirect_to root
+    end
+  end
+
+  def show
+    @song = Song.find_by(id: params[:id])
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(
+      :title, 
+      :artist, 
+      :key,
+      )
+  end
+
 end
