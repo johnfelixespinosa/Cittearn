4d008220d25f4461e0f1e23b8166e0f8bd0ec027
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-10-13 14:21:30 -0400

    User signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  def logged_in?
-    if !session[:user_id].present?
-      redirect_to '/login'
-    end
-  end
+  # def logged_in?
+  #   if !session[:user_id].present?
+  #     redirect_to '/login'
+  #   end
+  # end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id])
-  end
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id])
+  # end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,20 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :destroy]
-  before_action :logged_in?, only: [:show, :edit, :update, :destroy]
-  skip_before_action :verify_authenticity_token
+  # before_action :set_user, only: [:show, :destroy]
+  # before_action :logged_in?, only: [:show, :edit, :update, :destroy]
+
   
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      new_user_path
+    end
   end
 
   def show
@@ -15,24 +23,24 @@ class UsersController < ApplicationController
   def destroy
   end
 
-  private
+  # private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 
-  def current_user
-    User.find(session[:user_id])
-  end
+  # def current_user
+  #   User.find(session[:user_id])
+  # end
 
-  def user_params
-    params.require(:user).permit(
-      :name, 
-      :email, 
-      :password, 
-      :password_confirmation, 
-      :song_id, 
-      :comment_id
-      )
-  end
+  # def user_params
+  #   params.require(:user).permit(
+  #     :name, 
+  #     :email, 
+  #     :password, 
+  #     :password_confirmation, 
+  #     :song_id, 
+  #     :comment_id
+  #     )
+  # end
 end
