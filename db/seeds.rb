bdd540f0989bc930bc6063a71458ae445eaa9307
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-10-15 14:27:26 -0400

    Add users to seed file and songs have users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,31 @@
+User.create(
+  name:                  "AcousticLover",
+  email:                 "AcousticLover@gmail.com",
+  password:              "password123",
+  password_confirmation: "password123"
+)
+
+User.create(
+  name:                  "PoppySongsOnly",
+  email:                 "PoppySongsOnly@gmail.com",
+  password:              "password123",
+  password_confirmation: "password123"
+)
+
+User.create(
+  name:                  "Ballads4Lyfe",
+  email:                 "Ballads4Lyfe@gmail.com",
+  password:              "password123",
+  password_confirmation: "password123"
+)
+
+User.create(
+  name:                  "ShaniaTwain",
+  email:                 "ShaniaTwain@gmail.com",
+  password:              "password123",
+  password_confirmation: "password123"
+)
+
 Song.create(
   title:      "Read My Mind",
   artist:     "The Killers",
@@ -5,6 +33,7 @@ Song.create(
   tuning:     "Half Step Down",
   capo:       "No Capo",
   difficulty: "Intermediate",
+  user_id:    2,
   lyrics:     "On the corner of main street\r\nJust tryin' to keep it in line\r\nYou say you wanna move on and\r\nYou say I'm falling behind\r\n\r\nCan you read my mind?\r\nCan you read my mind?\r\n\r\nI never really gave up on\r\nBreakin' out of this two-star town\r\nI got the green light\r\nI got a little fight\r\nI'm gonna turn this thing around\r\n\r\nCan you read my mind?\r\nCan you read my mind?\r\n\r\nThe good old days, the honest man;\r\nThe restless heart, the Promised Land\r\nA subtle kiss that no one sees;\r\nA broken wrist and a big trapeze\r\n\r\nOh well I don't mind, if you don't mind\r\n'Cause I don't shine if you don't shine\r\nBefore you go, can you read my mind?\r\n\r\nIt’s funny how you just break down\r\nWaitin' on some sign\r\nI pull up to the front of your driveway\r\nWith magic soakin' my spine\r\n\r\nCan you read my mind?\r\nCan you read my mind?\r\n\r\nThe teenage queen, the loaded gun;\r\nThe drop dead dream, the Chosen One\r\nA southern drawl, a world unseen;\r\nA city wall and a trampoline\r\n\r\nOh well I don't mind, if you don't mind\r\n'Cause I don't shine if you don't shine\r\nBefore you jump\r\nTell me what you find when you read my mind\r\n\r\nSlippin’ in my faith until I fall\r\nYou never returned that call\r\nWoman, open the door, don't let it sting\r\nI wanna breathe that fire again\r\n\r\nShe said I don't mind, if you don't mind\r\n'Cause I don't shine if you don't shine\r\n\r\nPut your back on me\r\nPut your back on me\r\nPut your back on me\r\n\r\nThe stars are blazing like rebel diamonds cut out of the sun\r\nWhen you read my mind"
 )
 
@@ -15,6 +44,7 @@ Song.create(
   tuning:     "Standard",
   capo:       "No Capo",
   difficulty: "Expert",
+  user_id:    3,
   lyrics:     "Karma police, arrest this man\r\nHe talks in maths\r\nHe buzzes like a fridge\r\nHe's like a detuned radio\r\n\r\nKarma police, arrest this girl\r\nHer Hitler hairdo is\r\nMaking me feel ill\r\nAnd we have crashed her party\r\n\r\nThis is what you get\r\nThis is what you get\r\nThis is what you get when you mess with us\r\n\r\nKarma police\r\nI've given all I can\r\nIt's not enough\r\nI've given all I can\r\nBut we're still on the payroll\r\n\r\nThis is what you get\r\nThis is what you get\r\nThis is what you get when you mess with us\r\n\r\nFor a minute there I lost myself, I lost myself\r\nPhew, for a minute there I lost myself, I lost myself\r\n\r\nFor a minute there I lost myself, I lost myself\r\nPhew, for a minute there I lost myself, I lost myself"
 )
 
@@ -25,6 +55,7 @@ Song.create(
   tuning:     "Standard",
   capo:       "No Capo",
   difficulty: "Easy",
+  user_id:    1,
   lyrics:     "When you were here before\r\nCouldn't look you in the eye\r\nYou're just like an angel\r\nYour skin makes me cry\r\nYou float like a feather\r\nIn a beautiful world\r\nI wish I was special\r\nYou're so very special\r\n\r\nAnd I'm a creep\r\nI'm a weirdo\r\nWhat the hell am I doing here?\r\nI don't belong here\r\n\r\nI don't care if it hurts\r\nI want to have control\r\nI want a perfect body\r\nI want a perfect soul\r\nI want you to notice\r\nWhen I'm not around\r\nYou're so very special\r\nI wish I was special\r\n\r\nBut I'm a creep\r\nI'm a weirdo\r\nWhat the hell am I doing here?\r\nWhen I don't belong here\r\n\r\nOh, oh\r\n\r\nShe's running out again\r\nShe's running out\r\nShe run run run run\r\nRun\r\n\r\nWhatever makes you happy\r\nWhatever you want\r\nYou're so very special\r\nI wish I was special\r\n\r\nBut I'm a creep\r\nI'm a weirdo\r\nWhat the hell am I doing here?\r\nI don't belong here\r\nI don't belong here"
 )
 
@@ -35,6 +66,7 @@ Song.create(
   tuning:     "Standard",
   capo:       "3rd fret",
   difficulty: "Intermediate",
+  user_id:    2,
   lyrics:     "Oh yeah\r\nAlright\r\n\r\nSomebody's Heine'\r\nIs crowdin' my icebox\r\nSomebody's cold one\r\nIs givin' me chills\r\nGuess I'll just close my eyes\r\n\r\nOh yeah\r\nAlright\r\nFeels good\r\nInside\r\n\r\nFlip on the tele\r\nWrestle with Jimmy\r\nSomething is bubbling\r\nBehind my back\r\nThe bottle is ready to blow\r\n\r\nSay it ain't so\r\nYour drug is a heart-breaker\r\nSay it ain't so\r\nMy love is a life-taker\r\n\r\nI can't confront you\r\nI never could do\r\nThat which might hurt you\r\nSo try and be cool\r\nWhen I say\r\nThis way is a water-slide away from me\r\nThat takes you further every day\r\nSo be cool\r\n\r\nSay it ain't so\r\nYour drug is a heart-breaker\r\nSay it ain't so\r\nMy love is a life-taker\r\n\r\nDear Daddy\r\nI write you in spite of years of silence\r\nYou've cleaned up, found Jesus, things are good or so I hear\r\nThis bottle of Steven's awakens ancient feelings\r\nLike father, stepfather, the son is drowning in the flood\r\nYeah, yeah-yeah, yeah-yeah\r\n\r\nSay it ain't so\r\nYour drug is a heart-breaker\r\nSay it ain't so\r\nMy love is a life-taker\r\n\r\n"
 )
 
