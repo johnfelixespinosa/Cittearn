c565a3b710d8ecb33f6bc1db3f9429ed2734b9d4
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-10-17 12:49:46 -0400

    current_user and song_belongs_to_user? methods. Edit path for songsvalidate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,12 @@ private
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :logged_in?
+  def song_belongs_to_user?
+    current_user.id == @song.user_id
   end
+
+  helper_method :logged_in?
+  helper_method :current_user
+  helper_method :song_belongs_to_user?
+
+end
